6c965dd01da7c856f5e474b31c180341b7fe84e6
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2023-02-05 15:31:51 -0600

    Added logic to putDb and getDb exports in database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,38 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to database');
+    // Create connection to database
+    const contactDb = await openDB('jate', 1);
+    // Create transaction
+    const tx = contactDb.transaction('jate', 'readwrite');
+    // Open up object store
+    const store = tx.objectStore('jate');
+    // Use PUT method to pass in the content
+    const request = store.put({ id: 1, value: content });
+    // Confirmation of request
+    const result = await request;
+    console.log('Data saved to database', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
 
+
+export const getDb = async () => {
+  console.log('GET from database');
+  // Create connection to database
+  const contactDb = await openDB('jate', 1);
+  // Create transaction
+  const tx = contactDb.transaction('jate', 'readonly');
+  // Open up object store
+  const store = tx.objectStore('jate');
+  // Use getAll method to get all data
+  const request = store.getAll();
+  // Confirmation of request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+// Start the DB
 initdb();

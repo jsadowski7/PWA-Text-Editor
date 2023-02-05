375b0ad3135d0a61c3e2a68b83eac668fb0b7322
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2023-02-05 15:51:36 -0600

    Fixed typos in package.json, added console.log in install.js, fixed error in webpack.config.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,7 +2,9 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 window.addEventListener("beforeinstallprompt", (event) => {
-// Store triggered events
+  console.log("event" + event);
+  event.preventDefault();
+  // Store triggered events
   window.deferredPrompt = event;
   // Remove hidden class from button
   butInstall.classList.toggle("hidden", false);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,15 +3,16 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
@@ -21,7 +22,7 @@ module.exports = () => {
       // HTML plugin
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Text Editor'
+        title: 'JATE'
       }),
       // Inject Manifest plugin
       new InjectManifest({
@@ -32,7 +33,7 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Text Editor',
+        name: 'Just Another Text Editor',
         short_name: 'JATE',
         description: 'Just Another Text Editor',
         background_color: '#225ca3',
@@ -63,7 +64,7 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-res-spread', '@babel/transform-runtime'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },

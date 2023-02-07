82df52065db52d88d92044be2b54a01cf7e14bee
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2023-02-07 17:57:29 -0600

    Fixed syntax in database and install.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,48 +1,46 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('PUT to database');
-    // Create connection to database
-    const contactDb = await openDB('jate', 1);
-    // Create transaction
-    const tx = contactDb.transaction('jate', 'readwrite');
-    // Open up object store
-    const store = tx.objectStore('jate');
-    // Use PUT method to pass in the content
-    const request = store.put({ id: 1, value: content });
-    // Confirmation of request
-    const result = await request;
-    console.log('Data saved to database', result);
+  console.log("PUT to database");
+  // Create connection to database
+  const jateDb = await openDB("jate", 1);
+  // Create transaction
+  const tx = jateDb.transaction("jate", "readwrite");
+  // Open up object store
+  const store = tx.objectStore("jate");
+  // Use PUT method to pass in the content
+  const request = store.put({ jate: content });
+  // Confirmation of request
+  const result = await request;
+  console.log("Data saved to database", result);
 };
 
-
-
 export const getDb = async () => {
-  console.log('GET from database');
+  console.log("GET from database");
   // Create connection to database
-  const contactDb = await openDB('jate', 1);
+  const jateDb = await openDB("jate", 1);
   // Create transaction
-  const tx = contactDb.transaction('jate', 'readonly');
+  const tx = jateDb.transaction("jate", "readonly");
   // Open up object store
-  const store = tx.objectStore('jate');
+  const store = tx.objectStore("jate");
   // Use getAll method to get all data
   const request = store.getAll();
   // Confirmation of request
   const result = await request;
-  console.log('result.value', result);
+  console.log(result);
   return result;
 };
 
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,8 +2,6 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 window.addEventListener("beforeinstallprompt", (event) => {
-  console.log("event" + event);
-  event.preventDefault();
   // Store triggered events
   window.deferredPrompt = event;
   // Remove hidden class from button
